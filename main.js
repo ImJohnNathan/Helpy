b2b4dfa17f4a61759e5858bf184a7015ff9de91a
LambBoss <johnccortez2008@gmail.com>
2022-05-07 11:37:07 -0400

    ?help cmd

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
-
-const client = new Discord.Client()
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const token = 'OTcyNTA2MDc3MzIzMjE5MDQ0.YnaC3Q.RxwynOuIOHzqP5zrBm-zFXpfn8M'
 
 client.once('ready', () => {
     console.log('Helpy is online!');
 });
 
-client.login('OTcyNTA2MDc3MzIzMjE5MDQ0.YnaC3Q.RxwynOuIOHzqP5zrBm-zFXpfn8M')
+client.login(token)
+
+client.on('message', message => {
+    if(message.content.toLowerCase() === '?help') {
+        message.channel.send(`Hello ${message.author}! I'm helpy!`)
+    }
+})
